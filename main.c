5565f359c678004d6c18def091709948856eac4b
INDIANSNIPER24 <dev.deshmukh.in@gmail.com>
2025-11-06 15:49:34 +0530

    First

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <unistd.h>
+int main() {
+char name[100];
+
+
+	printf("Welcome To 2.5D Platformer\n\n\n");
+	printf("Hello Player!!\n");
+	sleep(3);
+	printf("i cee you are the another one reincarnated by Goddess of thsi World, isnt it?\n");
+	sleep(3);
+	printf("Well if you think that you are going to defeat the demon king and get Elfs Harem\n");
+	sleep(3);
+	printf("Then you are Absolutly Wrong\n");
+	sleep(3);
+	printf("now lets tell Me your Name Player:\n");
+	scanf("%s", name);
+	sleep(5);
+	printf("Welcome, %s! Welcome to your New Hell... i mean new Home\n\n", name);
+
+
+return(0);
+
+
+}
